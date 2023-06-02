20cd57fa381f91ec8fe195f38b02822f4ff6b096
kiranaryal <kiranaryal2057@gmail.com>
2023-06-02 18:58:26 +0545

    first commit

---
+++ resources/js/Pages/Quiz/Main.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+const props = defineProps({
+    quiz: {
+        type: Object,
+        default: () => ({}),
+    },
+});
+</script>
+
+<template>
+    <AppLayout title="Profile">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Quizs
+            </h2>
+        </template>
+
+        <div>a
+                <div v-for="q in quiz" >
+
+                        {{ q.name }}
+                </div>
+
+        </div>
+    </AppLayout>
+</template>
+
+
